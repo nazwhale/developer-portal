3ca1f004386da4f1369e2d8b64b8b44e6109544f
Naz <naz@monzo.com>
2018-07-06 11:36:58 +0100

    get auth working

--- src/App.js
+++ src/App.js
@@ -3,10 +3,30 @@ import "./App.css";
 import CardList from "./components/cardList";
 import Header from "./components/header";
 
+import AuthService from "./components/AuthService";
+import withAuth from "./components/withAuth";
+const Auth = new AuthService();
+
 class App extends Component {
+  handleLogout() {
+    console.log(this.props);
+    Auth.logout();
+    this.props.history.replace("/login");
+  }
+
   render() {
     return (
       <div className="App">
+        <h2>Welcome {this.props.user.username}</h2>
+        <p className="App-intro">
+          <button
+            type="button"
+            className="form-submit"
+            onClick={this.handleLogout.bind(this)}
+          >
+            Logout
+          </button>
+        </p>
         <Header />
         <CardList />
       </div>
@@ -14,4 +34,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withAuth(App);
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,41 @@
 import React, { Component } from "react";
 import "../Login.css";
+import AuthService from "./AuthService";
 
 class Login extends Component {
   constructor() {
     super();
     this.handleChange = this.handleChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.Auth = new AuthService();
   }
+
+  componentWillMount() {
+    if (this.Auth.loggedIn()) this.props.history.replace("/");
+  }
+
+  handleFormSubmit(e) {
+    e.preventDefault();
+
+    this.Auth.login(this.state.email, this.state.password)
+      .then(res => {
+        this.props.history.replace("/");
+      })
+      .catch(err => {
+        alert(err);
+      });
+  }
+
   render() {
     return (
       <div className="center">
         <div className="card">
           <h1>Login</h1>
-          <form>
+          <form onSubmit={this.handleFormSubmit}>
             <input
               className="form-item"
-              placeholder="Username goes here..."
-              name="username"
+              placeholder="Email goes here..."
+              name="email"
               type="text"
               onChange={this.handleChange}
             />
