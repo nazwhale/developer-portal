6a017828e871c0aef2c91648a628501fe91ebd26
Naz <naz@monzo.com>
2018-07-06 10:42:24 +0100

    add react router and login page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./index.css";
+import Login from "./components/Login";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route exact path="/login" component={Login} />
+    </div>
+  </Router>,
+  document.getElementById("root")
+);
 registerServiceWorker();
